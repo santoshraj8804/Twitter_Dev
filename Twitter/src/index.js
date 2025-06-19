f34182e77489bbddb74791b05a9d91c83ef371a9
Santosh raj <santoshrajgupta7366@gmail.com>
2025-06-19 20:47:32 +0530

    Started adding create service for tweets

--- Twitter/src/index.js
+++ Twitter/src/index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const connect = require('./config/database');
 const app = express();
 
+const Tweet = require('./models/tweet');
+
 app.listen(3000, async () => {
     console.log('Server started');
     await connect();
     console.log('Mongo DB connected');
+    const tweets = await Tweet.find({
+        content: ['First tweet', 'now its updated', '12344']
+    });
+    console.log(tweets);
 });
