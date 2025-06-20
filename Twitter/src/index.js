61199ea34e23196bf2da50c75a5ecd89705bf4b7
Santosh raj <santoshrajgupta7366@gmail.com>
2025-06-20 18:05:13 +0530

    Added tweet service with hashtag processing

--- Twitter/src/index.js
+++ Twitter/src/index.js
@@ -2,14 +2,8 @@ const express = require('express');
 const connect = require('./config/database');
 const app = express();
 
-const Tweet = require('./models/tweet');
-
 app.listen(3000, async () => {
     console.log('Server started');
     await connect();
     console.log('Mongo DB connected');
-    const tweets = await Tweet.find({
-        content: ['First tweet', 'now its updated', '12344']
-    });
-    console.log(tweets);
 });
--- Twitter/src/services/tweet-service.js
+++ Twitter/src/services/tweet-service.js
@@ -1,22 +1,27 @@
-const { TweetRepository } = require('../repository/index');
+const { TweetRepository, HashtagRepository } = require('../repository/index');
 
 class TweetService {
     constructor() {
         this.tweetRepository = new TweetRepository();
+        this.hashtagRepository = new HashtagRepository();
     }
 
     async create(data) {
         const content = data.content;
-        const tags = content.match(/#[a-zA-Z0-9_]+/g);
+        let tags = content.match(/#[a-zA-Z0-9_]+/g);
         tags = tags.map((tag) => tag.substring(1));
-        console.log(tags);
         const tweet = await this.tweetRepository.create(data);
-        // todo create hashtags and add here
-        /**
-         * 1. bulkcreate in mongoose
-         * 2. filter title of hashtag based on multiple tags
-         * 3. How to add tweet id inside all the hashtags
-         */
+        let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
+        let titleOfPresentTags = alreadyPresentTags.map(tags => tags.title);
+        let newTags = tags.filter(tag => !titleOfPresentTags.includes(tag));
+        newTags = newTags.map(tag => {
+             return {title: tag, tweets: [tweet._id]}
+        });
+        await this.hashtagRepository.bulkCreate(newTags);
+        alreadyPresentTags.forEach(tag => {
+            tag.tweets.push(tweet._id);
+            tag.save();
+        });
         return tweet;
     }
 }
